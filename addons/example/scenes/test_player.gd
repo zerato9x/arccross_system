3c39ddd212f15b6620e0e43be99bf6927f5f2555
zerato9x <zerato9x@gmail.com>
2026-07-17 01:00:05 +0700

    Phase 2 - First Refactor

--- addons/example/scenes/test_player.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-
-func _physics_process(_delta: float) -> void:
-	var input := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = 80.0 * input
-	move_and_slide()
